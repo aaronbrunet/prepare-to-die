16a1cbd50c38797f85e871be99365fc9eefef693
aaronbrunet <awbrunet@gmail.com>
2020-02-25 13:35:42 -0500

    Added randomisation + history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useReducer } from 'react';
+import styled from 'styled-components'
 import Card from './components/Card'
 import Result from './components/Result'
 import './App.css';
 
 function App() {
   const [roll,setRoll] = useState()
+  const [last,setLast] = useState([])
+
+  const LastRoll = styled.span`
+    margin: 0 10px;
+  `
 
   const diceList = 
   [{name: 'D4',sides: 4},
@@ -15,7 +21,11 @@ function App() {
   {name: 'D20',sides: 20}]
 
   const rolled = dice => {
-    setRoll(dice.sides)
+    const min = 1
+    const max = dice.sides
+    const rand = Math.round(min + Math.random() * (max - min))
+    setRoll(rand)
+    setLast([...last,rand])
   }
 
 
@@ -23,6 +33,14 @@ function App() {
     <div className="App">
       {diceList.map((die,index)=>
         <Card die={die} rolled={rolled} key={index}/>
+      )}
+      {last.length > 1 && (
+        <> 
+      <h4>Last rolled:</h4>
+      {last.map((roll,index)=>
+        <LastRoll key={index}>{roll}{last.length > 0 && index < last.length-1 && (',') }</LastRoll>
+      )}
+      </>
       )}
       <Result roll={roll} />
     </div>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 const Result = (props) => {
-    //const [roll,setRoll] = useState(props.roll)
+        
     return (
     <>
         <h4>Roll something!</h4>
-        <h4>{props.roll}</h4>
+        <h4>{props.roll}</h4>        
     </>
     )
 }
